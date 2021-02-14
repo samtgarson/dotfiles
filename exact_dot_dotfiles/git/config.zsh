985a738f158ad3cb472b73f82554493061d386f6
Sam Garson <samtgarson@gmail.com>
2021-02-14 21:29:38 +0000

    Git config and clean up aliases

--- exact_dot_dotfiles/git/config.zsh
+++ exact_dot_dotfiles/git/config.zsh
@@ -1,8 +1,14 @@
+source ~/.local/share/chezmoi/.helpers
+
 # aliases
-alias gprune='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d && git remote prune origin'
+alias gprune='git branch --merged | egrep -v "(^\*|main|master|dev)" | xargs git branch -d && git remote prune origin'
 alias gwip='gaa && gc -m "wip" -n'
 alias gdc='gd --cached'
 alias gai='git add -i'
+
+function default-branch () {
+  git symbolic-ref refs/remotes/origin/HEAD | splt 4 '/'
+}
 
 # pull master and prune, and return
 function gupdate () {
@@ -10,7 +16,36 @@ function gupdate () {
   then
     gco $1 && gl && gprune && gco -
   else
-    gco master && gl && gprune && gco -
+    local target=$(default-branch)
+    gco $target && gl && gprune && gco -
   fi
 }
 
+# gco - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
+fbr() {
+  if [[ -n $1 ]]; then
+    git checkout $1
+    return
+  fi
+
+  local branches branch current_branch
+  current_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" | grep -wv $current_branch |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
+alias gco='fbr'
+
+# get a sha from the logs
+sha() {
+  glol --color=always \
+    | fzf --ansi \
+    | remove_colour \
+    | sed -nr  "s/^.* ([0-9a-z]{7}) - .*$/\1/p"
+}
+
+# gfix - choose commit to fixup
+gfix() {
+  git commit --no-verify --fixup `sha`
+}
--- exact_dot_dotfiles/productivity/config.zsh
+++ exact_dot_dotfiles/productivity/config.zsh
@@ -18,37 +18,6 @@ _fzf_compgen_dir() {
   fd --type d --hidden --follow --exclude ".git" --exclude "node_modules" . "$1"
 }
 
-# gco - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-fbr() {
-  if [[ -n $1 ]]; then
-    git checkout $1
-    return
-  fi
-
-  local branches branch current_branch
-  current_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
-  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-  branch=$(echo "$branches" | grep -wv $current_branch |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
-alias gco='fbr'
-
-alias remove_colour="sed 's/\x1b\[[0-9;]*m//g'"
-
-# get a sha from the logs
-sha() {
-  glol --color=always \
-    | fzf --ansi \
-    | remove_colour \
-    | sed -nr  "s/^.* ([0-9a-z]{7}) - .*$/\1/p"
-}
-
-# gfix - choose commit to fixup
-gfix() {
-  git commit --no-verify --fixup `sha`
-}
-
 kill-port () {
   local processes
 
@@ -69,5 +38,7 @@ kill-port () {
 
 # splt - that awk thing that is annoying to type
 splt () {
-  awk -F ' ' "{ print \$$1 }"
+  local index=${1:-1}
+  local divider=${2:-" "}
+  awk -F $divider "{ print \$$index }"
 }
