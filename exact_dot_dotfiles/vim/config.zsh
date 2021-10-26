4a74699711ac7a69adb26a9e58ef41b8342eeaef
Sam Garson <samtgarson@gmail.com>
2021-10-26 22:52:38 +0100

    Fix some nvim remote config for git editing

--- exact_dot_dotfiles/vim/config.zsh
+++ exact_dot_dotfiles/vim/config.zsh
@@ -2,14 +2,14 @@
 
 source ~/.local/share/chezmoi/.helpers
 
-alias nvim="NVIM_LISTEN_ADDRESS=./.nvimsocket nvim"
+# alias nvim="NVIM_LISTEN_ADDRESS=./.nvimsocket nvim"
 alias vim=nvim
 alias vi=nvim
 alias v="vim ."
 
-if is_installed 'nvr'; then
-  export GIT_EDITOR="nvr --servername .nvimsocket --remote-tab-wait -s"
-else
-  export GIT_EDITOR="nvim"
-fi
+# if is_installed 'nvr'; then
+#   export GIT_EDITOR="nvr --servername .nvimsocket --remote-tab-wait -s"
+# else
+#   export GIT_EDITOR="nvim"
+# fi
 
