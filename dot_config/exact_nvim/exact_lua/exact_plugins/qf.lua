2ad8e519ea52ea07ac231c5762e3155724f6cb19
Sam Garson <samtgarson@gmail.com>
2024-01-15 10:31:21 +0000

    Install prettier quickfix list

--- dot_config/exact_nvim/exact_lua/exact_plugins/qf.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/qf.lua
@@ -1,11 +1,14 @@
 return {
-  'ten3roberts/qf.nvim',
-  dependencies = { 'romainl/vim-qf' },
-  ft = "qf",
-  keys = {
-    { ']q',           '<Plug>(qf_qf_next)',                    mode = 'n', desc = 'Next quickfix item' },
-    { '[q',           '<Plug>(qf_qf_previous)',                mode = 'n', desc = 'Previous quickfix item' },
-    { '<Leader><CR>', function() require 'qf'.toggle('c') end, mode = 'n', desc = 'Toggle quickfix window', silent = true }
+  {
+    'ten3roberts/qf.nvim',
+    dependencies = { 'romainl/vim-qf' },
+    ft = "qf",
+    keys = {
+      { ']q',           '<Plug>(qf_qf_next)',                    mode = 'n', desc = 'Next quickfix item' },
+      { '[q',           '<Plug>(qf_qf_previous)',                mode = 'n', desc = 'Previous quickfix item' },
+      { '<Leader><CR>', function() require 'qf'.toggle('c') end, mode = 'n', desc = 'Toggle quickfix window', silent = true }
+    },
+    config = true
   },
-  config = true
+  { "yorickpeterse/nvim-pqf", ft = "qf" },
 }
