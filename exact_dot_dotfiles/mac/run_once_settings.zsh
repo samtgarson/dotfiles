5627708c81453efa11a3161912ee1d34cd93a703
Sam Garson <samtgarson@gmail.com>
2024-04-10 10:20:26 +0100

    Set default mac os currency to GBP

--- exact_dot_dotfiles/mac/run_once_settings.zsh
+++ exact_dot_dotfiles/mac/run_once_settings.zsh
@@ -63,7 +63,7 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Set language and text formats
 defaults write NSGlobalDomain AppleLanguages -array "en" "nl"
-defaults write NSGlobalDomain AppleLocale -string "en_GB@currency=EUR"
+defaults write NSGlobalDomain AppleLocale -string "en_GB@currency=GBP"
 defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
 defaults write NSGlobalDomain AppleMetricUnits -bool true
 
