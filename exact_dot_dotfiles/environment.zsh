1da036e5d192ba05e4ea48efc136e4c0eea391a8
Sam Garson <samtgarson@gmail.com>
2026-01-28 16:58:34 +0000

    More zed config

--- exact_dot_dotfiles/environment.zsh
+++ exact_dot_dotfiles/environment.zsh
@@ -5,6 +5,6 @@ export ANDROID_SDK=$HOME/Library/Android/sdk
 export ANDROID_HOME="$ANDROID_SDK"
 export PATH=$HOME/Library/Android/sdk/platform-tools:$PATH
 
-export EDITOR=vim
+export EDITOR=zed
 
 export NODEJS_CHECK_SIGNATURES=no
